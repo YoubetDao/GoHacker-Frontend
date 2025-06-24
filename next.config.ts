71f405ccc2e67dc6bc9511782c38ea6f238204d2
null0x62 <guisumail@163.com>
2025-06-24 22:52:29 +0900

    fix:vercel serverless

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/api/leaderboard/builders',
+        destination: 'https://api.hunknownz.xyz:2096/leaderboard/builders',
+      },
+      {
+        source: '/api/leaderboard/builders/:path*',
+        destination: 'https://api.hunknownz.xyz:2096/leaderboard/builders/:path*',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
